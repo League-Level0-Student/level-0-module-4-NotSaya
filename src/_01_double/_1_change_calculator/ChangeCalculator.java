5099fb09107342ac6fa7be6390fa74db8d9ed69b
root <root@49bef44dd1b4>
2021-01-16 02:25:59 +0000

    Started Module 4

--- src/_01_double/_1_change_calculator/ChangeCalculator.java
+++ src/_01_double/_1_change_calculator/ChangeCalculator.java
@@ -12,17 +12,18 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+String nickels = JOptionPane.showInputDialog("How many nickels do you have?");
 		// Convert their answer to an int.   Hint: Integer.parseInt()  
-
+int Nickels = Integer.parseInt(nickels);
 		// Ask the user how many dimes they have, and convert their answer
-
+String dime = JOptionPane.showInputDialog("How many dimes do you have?");
+int dimes = Integer.parseInt(dime);
 		// Ask the user how many quarters they have, and convert their answer
-
+String quarter = JOptionPane.showInputDialog("How many quarters do you have?");
+int quarters = Integer.parseInt(quarter);
 		// Calculate how much money the user has.  Hint: Use a double variable 
-
+double money = quarters * 25 + dimes * 10 + Nickels * 5;
 		// Tell the user how much money they have in dollars and cents format (e.g. $6.75)
-
+JOptionPane.showMessageDialog(null, "You have $" + money/100);
 	}
 }
-
--- src/_02_boolean/_1_sleepy_head/SleepyHead.java
+++ src/_02_boolean/_1_sleepy_head/SleepyHead.java
@@ -12,28 +12,46 @@ public class SleepyHead {
 		/************************  SLEEPY HEAD  **************************/
 		boolean isWeekend;
 		// Write code to ask the user what day it is.
-        
+        String day = JOptionPane.showInputDialog("What day is it today?");
 		// Set the boolean isWeekend based on the value they enter
-		
+		if (day.equalsIgnoreCase("Sunday") || day.equalsIgnoreCase("Saturday")) {
+			isWeekend = true;
+		}
+		else  {
+		isWeekend = false;
+		}
 		
 		// If it is the weekend, tell the user they get to sleep in.
-		
+		if (isWeekend == true) {
+			JOptionPane.showMessageDialog(null, "You get to sleep in!");
+		}
 		// If it is not the weekend, tell them to get out of bed and go to school!
-		
+		else if (isWeekend == false) {
+			JOptionPane.showMessageDialog(null, "Get out of bed and go to school!");
+		}
 		
 		/************************   STAR STUDENT  **************************/
 
 		boolean passedExam;
 		// Write code to ask the user what percentage they scored in their last exam
-
-		
+String percentage = JOptionPane.showInputDialog("What percentage did you score on your last exam?");
+		double score = Integer.parseInt(percentage);
 		// If they scored more than 70, they passed the exam.
 		// Set the boolean passedExam based on their score.
-		
+		if (score > 70) {
+			passedExam = true;
+		}
+		else {
+			passedExam = false;
+		}
 		// If the user passed the exam, congratulate them
-		
+		if (passedExam == true) {
+			JOptionPane.showMessageDialog(null, "Congratulations!");
+		}
 		// otherwise, wish them better luck next time.
-		
+		if (passedExam == false) {
+			JOptionPane.showMessageDialog(null, "I hope you do better next time!");
+		}
 		
 		/************************  GAME OVER  **************************/
 
@@ -55,23 +73,40 @@ public class SleepyHead {
 		
 		boolean isRed;
 		// Ask the user what color to draw with. Based on their answer, set the isRed variable
-		
-		
+		String color = JOptionPane.showInputDialog("What color would you like to draw with?");
+		if (color.equalsIgnoreCase("red")) {
+			isRed = true;
+		}
+		else {
+			isRed = false;
+		}
 		boolean isSquare;		
 		// Now ask the user what shape to draw. Based on their answer, set the isSquare variable
+		String shape = JOptionPane.showInputDialog("What shape should be drawn?");
+			if (shape.equalsIgnoreCase("square")) {
+				isSquare = true;
+			}
+			else {
+				isSquare = false;
+			}
 		
 		
 		// Now you MUST use the && operator to join the booleans for this code. 
 		// Ask your teacher if you are not sure how
 		// Use an if statement to ONLY draw a red square when it has been requested (use the method below)
+		if (isRed == true && isSquare == true) {
+			drawRedSquare();
+		}
 		// otherwise, tell the user you don't know how to draw that shape
-		
+		else {
+			JOptionPane.showMessageDialog(null, "I don't know how to draw that shape.");
+		}
 
 	}
 
 	
 	static void drawRedSquare() {
 		Robot rob = new Robot();
-		// Complete the rest of this method
+		
 	}	
 }
